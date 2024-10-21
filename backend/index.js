a08081de82bb6b1dfa797d1b1c35665511fe6b0a
Cyril Stafford Giri <cyrilgiri001@gmail.com>
2024-10-21 13:05:25 +0530

    create routes

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 const app = express();
 
 app.use(express.json());
 
 app.get('/todos', (req, res)=>{
-    res.send("hello world!")
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You have sent the wrong input"
+        })
+        return;
+    }
+
+    //mongodb logic
 })
 
 app.post('/todos', function(req, res){
@@ -12,7 +22,14 @@ app.post('/todos', function(req, res){
 })
 
 app.put('/completed', function(req, res){
-
+    const updatePayload = req.body;
+    const parsedPayload = updateTodo.safeParse(updatePayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg: "You have sent the wrong input"
+        })
+        return;
+    }
 })
 
 app.delete('/todos', function(req, res){
